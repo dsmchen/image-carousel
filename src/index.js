1ef816922b7d77837e27247b609b152a9169e499
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-09-29 14:17:07 +0800

    Add dotnav functionality

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ function nextImage() {
   const compStyles = window.getComputedStyle(innerContainer);
   const left = compStyles.getPropertyValue("left");
 
+  const dotnavItems = document.querySelectorAll(".dotnav-item");
+  dotnavItems.forEach((item) => {
+    item.classList.remove("current");
+  });
+
   if (left === "0px") {
     innerContainer.style.left = "-400px";
+    dotnavItems[1].classList.add("current");
   } else if (left === "-400px") {
     innerContainer.style.left = "-800px";
+    dotnavItems[2].classList.add("current");
   } else if (left === "-800px") {
-    innerContainer.style.left = "0";
+    innerContainer.style.left = 0;
+    dotnavItems[0].classList.add("current");
   }
 }
 
@@ -19,12 +27,37 @@ function previousImage() {
   const compStyles = window.getComputedStyle(innerContainer);
   const left = compStyles.getPropertyValue("left");
 
+  const dotnavItems = document.querySelectorAll(".dotnav-item");
+  dotnavItems.forEach((item) => {
+    item.classList.remove("current");
+  });
+
   if (left === "0px") {
     innerContainer.style.left = "-800px";
+    dotnavItems[2].classList.add("current");
   } else if (left === "-400px") {
-    innerContainer.style.left = "0";
+    innerContainer.style.left = 0;
+    dotnavItems[0].classList.add("current");
   } else if (left === "-800px") {
     innerContainer.style.left = "-400px";
+    dotnavItems[1].classList.add("current");
+  }
+}
+
+function handleClickDotnav(e) {
+  const dotnavItems = document.querySelectorAll(".dotnav-item");
+  dotnavItems.forEach((item) => {
+    item.classList.remove("current");
+  });
+  e.target.classList.add("current");
+
+  const innerContainer = document.querySelector(".inner-container");
+  if (e.target.classList.contains("dotnav-item-1")) {
+    innerContainer.style.left = 0;
+  } else if (e.target.classList.contains("dotnav-item-2")) {
+    innerContainer.style.left = "-400px";
+  } else if (e.target.classList.contains("dotnav-item-3")) {
+    innerContainer.style.left = "-800px";
   }
 }
 
@@ -34,4 +67,10 @@ function previousImage() {
 
   nextButton.addEventListener("click", nextImage);
   previousButton.addEventListener("click", previousImage);
+
+  const dotnavItems = document.querySelectorAll(".dotnav-item");
+
+  dotnavItems.forEach((item) => {
+    item.addEventListener("click", handleClickDotnav);
+  });
 })();

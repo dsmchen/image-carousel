3bc4f77b7491e57a03a5312e4a3e7647092b8e91
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-09-29 11:51:48 +0800

    Add next and previous slide functionality

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - Font: [Inclusive Sans](https://fonts.google.com/specimen/Inclusive+Sans)
 - CSS reset: [Josh's Custom CSS Reset](https://www.joshwcomeau.com/css/custom-css-reset/)
 - Images: [Ella Olsson](https://unsplash.com/@ellaolsson?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash) on [Unsplash](https://unsplash.com/)
+- Icons: [Lucide](https://lucide.dev/)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./style.css";
+
+function nextImage() {
+  const innerContainer = document.querySelector(".inner-container");
+  const compStyles = window.getComputedStyle(innerContainer);
+  const left = compStyles.getPropertyValue("left");
+
+  if (left === "0px") {
+    innerContainer.style.left = "-400px";
+  } else if (left === "-400px") {
+    innerContainer.style.left = "-800px";
+  } else if (left === "-800px") {
+    innerContainer.style.left = "0";
+  }
+}
+
+function previousImage() {
+  const innerContainer = document.querySelector(".inner-container");
+  const compStyles = window.getComputedStyle(innerContainer);
+  const left = compStyles.getPropertyValue("left");
+
+  if (left === "0px") {
+    innerContainer.style.left = "-800px";
+  } else if (left === "-400px") {
+    innerContainer.style.left = "0";
+  } else if (left === "-800px") {
+    innerContainer.style.left = "-400px";
+  }
+}
+
+(function () {
+  const nextButton = document.querySelector(".next");
+  const previousButton = document.querySelector(".previous");
+
+  nextButton.addEventListener("click", nextImage);
+  previousButton.addEventListener("click", previousImage);
+})();
